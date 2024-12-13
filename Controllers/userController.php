cfc94607b146b9a6d526dc4c5dbf997d5645b8b2
Hashual <loizeau.dorian05@gmail.com>
2024-12-13 09:39:40 +0100

    add function for profil

--- Controllers/userController.php
+++ Controllers/userController.php
@@ -17,4 +17,23 @@ class userController {
     function connexion() {
         require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'Users'.DIRECTORY_SEPARATOR.'login.php');
     }
+
+    function profil() {
+        if (isset($_SESSION['id']) && isset($_SESSION['mail'])) {
+            $requete = $this->pdo->prepare('SELECT * FROM users WHERE id = :id AND mail = :mail');
+            $requete->bindParam(':id', $_SESSION['id']);
+            $requete->bindParam(':mail', $_SESSION['mail']);
+            $requete->execute();
+            $user = $requete->fetch(PDO::FETCH_ASSOC);
+    
+            if ($user) {
+                require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'Users' . DIRECTORY_SEPARATOR . 'profil.php');
+            } else {
+                echo "Utilisateur non trouvé.";
+            }
+        } else {
+            header('Location: ?c=login');
+            exit();
+        }
+    }
 }
